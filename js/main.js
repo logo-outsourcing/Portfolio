d2246be09f342a6c78074e6015f18fe0c96b68e2
= <vikytrusowa.@gmail.com>
2020-05-14 17:06:56 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$('.block__link').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+    return false;
+});
+$('.header__link').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+    return false;
+});
+
 var canvas = document.createElement("canvas");
 c = canvas.getContext('2d');
 
@@ -10,7 +21,7 @@ document.body.appendChild(canvas);
 (function loop() {
 
     for (var i = 0, a = imageData.data.length; i < a; i++) {
-        imageData.data[i] = (Math.random() * 255)|0;
+        imageData.data[i] = (Math.random() * 255) | 0;
     }
 
     c.putImageData(imageData, 0, 0);
@@ -21,7 +32,7 @@ document.body.appendChild(canvas);
 var $input = $('.contact__input'),
     $buffer = $('.contact__input--buffer');
 
-$input.on('input', function() {
+$input.on('input', function () {
     $buffer.text($input.val());
     $input.width($buffer.width());
 });
