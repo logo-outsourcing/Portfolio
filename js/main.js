2d813df8e3a2e9c14a1eddef6a5f3ddf0c8c0cb7
= <vikytrusowa.@gmail.com>
2020-05-15 13:29:22 +0300

    editing page adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function () {
+        posX += (mouseX - posX) / 9;
+        posY += (mouseY - posY) / 9;
+
+        TweenMax.set(follower, {
+            css: {
+                left: posX - 12,
+                top: posY - 12
+            }
+        });
+
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        });
+    }
+});
+
+$(document).on("mousemove", function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+// $(".link").on("mouseenter", function() {
+//     cursor.addClass("active");
+//     follower.addClass("active");
+// });
+// $(".link").on("mouseleave", function() {
+//     cursor.removeClass("active");
+//     follower.removeClass("active");
+// });
+
+
+$(".work__link").on("mouseenter", function () {
+    cursor.addClass("active2");
+    follower.addClass("active2");
+});
+$(".work__link").on("mouseleave", function () {
+    cursor.removeClass("active2");
+    follower.removeClass("active2");
+});
+
+
 $('.block__link').click(function () {
     var target = $(this).attr('href');
     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
@@ -36,3 +92,7 @@ $input.on('input', function () {
     $buffer.text($input.val());
     $input.width($buffer.width());
 });
+
+
+
+
