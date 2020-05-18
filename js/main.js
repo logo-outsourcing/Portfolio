3bb5f6a50c23804209f0b06facc360c1bed3163b
= <vikytrusowa.@gmail.com>
2020-05-18 12:25:48 +0300

    editing animate title

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+    $(function () {
+        $('.parallax').paroller();
 
+    });
     // $(document).on( 'scroll', function(){
     //     var scrollHeight = Math.max(
     //         document.body.scrollHeight, document.documentElement.scrollHeight,
@@ -12,41 +15,41 @@ $(document).ready(function () {
 
 
     $('.header__title--element').addClass('header__title--active')
-var cursor = $(".cursor"),
-    follower = $(".cursor-follower");
+    var cursor = $(".cursor"),
+        follower = $(".cursor-follower");
 
-var posX = 0,
-    posY = 0;
+    var posX = 0,
+        posY = 0;
 
-var mouseX = 0,
-    mouseY = 0;
+    var mouseX = 0,
+        mouseY = 0;
 
-TweenMax.to({}, 0.016, {
-    repeat: -1,
-    onRepeat: function () {
-        posX += (mouseX - posX) / 9;
-        posY += (mouseY - posY) / 9;
+    TweenMax.to({}, 0.016, {
+        repeat: -1,
+        onRepeat: function () {
+            posX += (mouseX - posX) / 9;
+            posY += (mouseY - posY) / 9;
 
-        TweenMax.set(follower, {
-            css: {
-                left: posX - 12,
-                top: posY - 12
-            }
-        });
+            TweenMax.set(follower, {
+                css: {
+                    left: posX - 12,
+                    top: posY - 12
+                }
+            });
 
-        TweenMax.set(cursor, {
-            css: {
-                left: mouseX,
-                top: mouseY
-            }
-        });
-    }
-});
+            TweenMax.set(cursor, {
+                css: {
+                    left: mouseX,
+                    top: mouseY
+                }
+            });
+        }
+    });
 
-$(document).on("mousemove", function (e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-});
+    $(document).on("mousemove", function (e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+    });
 
 // $(".link").on("mouseenter", function() {
 //     cursor.addClass("active");
@@ -58,55 +61,54 @@ $(document).on("mousemove", function (e) {
 // });
 
 
-$(".work__link").on("mouseenter", function () {
-    cursor.addClass("active2");
-    follower.addClass("active2");
-});
-$(".work__link").on("mouseleave", function () {
-    cursor.removeClass("active2");
-    follower.removeClass("active2");
-});
+    $(".work__link").on("mouseenter", function () {
+        cursor.addClass("active2");
+        follower.addClass("active2");
+    });
+    $(".work__link").on("mouseleave", function () {
+        cursor.removeClass("active2");
+        follower.removeClass("active2");
+    });
 
 
-$('.block__link').click(function () {
-    var target = $(this).attr('href');
-    $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-    return false;
-});
-$('.header__link').click(function () {
-    var target = $(this).attr('href');
-    $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-    return false;
-});
+    $('.block__link').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+        return false;
+    });
+    $('.header__link').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+        return false;
+    });
 
-var canvas = document.createElement("canvas");
-c = canvas.getContext('2d');
+    var canvas = document.createElement("canvas");
+    c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
-var imageData = c.createImageData(canvas.width, canvas.height);
-document.body.appendChild(canvas);
+    var imageData = c.createImageData(canvas.width, canvas.height);
+    document.body.appendChild(canvas);
 
-(function loop() {
+    (function loop() {
 
-    for (var i = 0, a = imageData.data.length; i < a; i++) {
-        imageData.data[i] = (Math.random() * 255) | 0;
-    }
+        for (var i = 0, a = imageData.data.length; i < a; i++) {
+            imageData.data[i] = (Math.random() * 255) | 0;
+        }
 
-    c.putImageData(imageData, 0, 0);
-    requestAnimationFrame(loop);
+        c.putImageData(imageData, 0, 0);
+        requestAnimationFrame(loop);
 
-})();
+    })();
 
-var $input = $('.contact__input'),
-    $buffer = $('.contact__input--buffer');
+    var $input = $('.contact__input'),
+        $buffer = $('.contact__input--buffer');
 
-$input.on('input', function () {
-    $buffer.text($input.val());
-    $input.width($buffer.width());
-});
-
+    $input.on('input', function () {
+        $buffer.text($input.val());
+        $input.width($buffer.width());
+    });
 
 
 });
