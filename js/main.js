1ea3262c88e69df0713059315934b357de23d884
= <vikytrusowa.@gmail.com>
2020-05-19 12:07:06 +0300

    editing animation

--- js/main.js
+++ js/main.js
@@ -1,52 +1,75 @@
 $(document).ready(function () {
 
 
-    $('.header__title--element').addClass('header__title--active');
-    var cursor = $(".cursor"),
-        follower = $(".cursor-follower");
+    (function () {
+        var scroll = new LocomotiveScroll({
+            el: document.querySelector('#js-scroll'),
+            smooth: true,
+            smoothMobile: true,
+            touchMultiplier: 2,
+            getSpeed: true,
+            getDirection: true,
+            inertia: 1,
+            firefoxMultiplier: 50,
+            useKeyboard: true,
+            offset: [0, 0],
+            repeat: true,
+            class: "is-inview",
+            scrollbarClass: "c-scrollbar",
+            scrollingClass: "has-scroll-scrolling",
+            draggingClass: "has-scroll-dragging",
+            smoothClass: "has-scroll-smooth",
+            initClass: "has-scroll-init",
+        });
 
-    var posX = 0,
-        posY = 0;
-
-    var mouseX = 0,
-        mouseY = 0;
-
-    TweenMax.to({}, 0.016, {
-        repeat: -1,
-        onRepeat: function () {
-            posX += (mouseX - posX) / 9;
-            posY += (mouseY - posY) / 9;
-
-            TweenMax.set(follower, {
-                css: {
-                    left: posX - 12,
-                    top: posY - 12
-                }
-            });
-
-            TweenMax.set(cursor, {
-                css: {
-                    left: mouseX,
-                    top: mouseY
-                }
-            });
-        }
-    });
-
-    $(document).on("mousemove", function (e) {
-        mouseX = e.pageX;
-        mouseY = e.pageY;
-    });
+    })();
 
 
-    $(".work__link").on("mouseenter", function () {
-        cursor.addClass("active2");
-        follower.addClass("active2");
-    });
-    $(".work__link").on("mouseleave", function () {
-        cursor.removeClass("active2");
-        follower.removeClass("active2");
-    });
+    // var cursor = $(".cursor"),
+    //     follower = $(".cursor-follower");
+    //
+    // var posX = 0,
+    //     posY = 0;
+    //
+    // var mouseX = 0,
+    //     mouseY = 0;
+    //
+    // TweenMax.to({}, 0.016, {
+    //     repeat: -1,
+    //     onRepeat: function () {
+    //         posX += (mouseX - posX) / 9;
+    //         posY += (mouseY - posY) / 9;
+    //
+    //         TweenMax.set(follower, {
+    //             css: {
+    //                 left: posX - 12,
+    //                 top: posY - 12
+    //             }
+    //         });
+    //
+    //         TweenMax.set(cursor, {
+    //             css: {
+    //                 left: mouseX,
+    //                 top: mouseY
+    //             }
+    //         });
+    //     }
+    // });
+    //
+    // $(document).on("mousemove", function (e) {
+    //     mouseX = e.pageX;
+    //     mouseY = e.pageY;
+    // });
+    //
+    //
+    // $(".work__link").on("mouseenter", function () {
+    //     cursor.addClass("active2");
+    //     follower.addClass("active2");
+    // });
+    // $(".work__link").on("mouseleave", function () {
+    //     cursor.removeClass("active2");
+    //     follower.removeClass("active2");
+    // });
 
 
     $('.block__link').click(function () {
@@ -91,3 +114,4 @@ $(document).ready(function () {
 
 });
 
+//$('.header__title--element').addClass('header__title--active');
