fd9d13336738d7234c24e152baaf8d48853e6c51
= <vikytrusowa.@gmail.com>
2020-05-19 14:02:32 +0300

    remove scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+$('.content-site').removeClass('hidden');
     (function () {
         var scroll = new LocomotiveScroll({
             el: document.querySelector('#js-scroll'),
@@ -23,54 +23,6 @@ $(document).ready(function () {
         });
 
     })();
-
-
-    // var cursor = $(".cursor"),
-    //     follower = $(".cursor-follower");
-    //
-    // var posX = 0,
-    //     posY = 0;
-    //
-    // var mouseX = 0,
-    //     mouseY = 0;
-    //
-    // TweenMax.to({}, 0.016, {
-    //     repeat: -1,
-    //     onRepeat: function () {
-    //         posX += (mouseX - posX) / 9;
-    //         posY += (mouseY - posY) / 9;
-    //
-    //         TweenMax.set(follower, {
-    //             css: {
-    //                 left: posX - 12,
-    //                 top: posY - 12
-    //             }
-    //         });
-    //
-    //         TweenMax.set(cursor, {
-    //             css: {
-    //                 left: mouseX,
-    //                 top: mouseY
-    //             }
-    //         });
-    //     }
-    // });
-    //
-    // $(document).on("mousemove", function (e) {
-    //     mouseX = e.pageX;
-    //     mouseY = e.pageY;
-    // });
-    //
-    //
-    // $(".work__link").on("mouseenter", function () {
-    //     cursor.addClass("active2");
-    //     follower.addClass("active2");
-    // });
-    // $(".work__link").on("mouseleave", function () {
-    //     cursor.removeClass("active2");
-    //     follower.removeClass("active2");
-    // });
-
 
     $('.block__link').click(function () {
         var target = $(this).attr('href');
@@ -114,4 +66,4 @@ $(document).ready(function () {
 
 });
 
-//$('.header__title--element').addClass('header__title--active');
+$('.header__title--element').addClass('header__title--active');
