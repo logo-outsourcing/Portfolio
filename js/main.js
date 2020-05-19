0c17e9f7100079a596761a4b74b25f3da01b34ec
= <vikytrusowa.@gmail.com>
2020-05-19 15:14:03 +0300

    remove canvas

--- js/main.js
+++ js/main.js
@@ -17,25 +17,7 @@ $(document).ready(function () {
         return false;
     });
 
-    var canvas = document.createElement("canvas");
-    c = canvas.getContext('2d');
 
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-
-    var imageData = c.createImageData(canvas.width, canvas.height);
-    document.body.appendChild(canvas);
-
-    (function loop() {
-
-        for (var i = 0, a = imageData.data.length; i < a; i++) {
-            imageData.data[i] = (Math.random() * 255) | 0;
-        }
-
-        c.putImageData(imageData, 0, 0);
-        requestAnimationFrame(loop);
-
-    })();
 
     var $input = $('.contact__input'),
         $buffer = $('.contact__input--buffer');
