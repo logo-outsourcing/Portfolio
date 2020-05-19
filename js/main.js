b4c9a9ae7d2868f4e34b96c89d75c3e9f3da9a3d
= <vikytrusowa.@gmail.com>
2020-05-19 15:32:01 +0300

    add scroll

--- js/main.js
+++ js/main.js
@@ -4,6 +4,28 @@ $(document).ready(function () {
     $('.content-site').removeClass('hidden');
     setTimeout(function () {
         $('.header__title--element').addClass('header__title--active');
+        (function () {
+            var scroll = new LocomotiveScroll({
+                el: document.querySelector('#js-scroll'),
+                smooth: true,
+                smoothMobile: true,
+                touchMultiplier: 2,
+                getSpeed: true,
+                getDirection: true,
+                inertia: 1,
+                firefoxMultiplier: 50,
+                useKeyboard: true,
+                offset: [0, 0],
+                repeat: true,
+                class: "is-inview",
+                scrollbarClass: "c-scrollbar",
+                scrollingClass: "has-scroll-scrolling",
+                draggingClass: "has-scroll-dragging",
+                smoothClass: "has-scroll-smooth",
+                initClass: "has-scroll-init",
+            });
+
+        })();
     }, 500);
 
     $('.block__link').click(function () {
@@ -16,8 +38,6 @@ $(document).ready(function () {
         $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
         return false;
     });
-
-
 
     var $input = $('.contact__input'),
         $buffer = $('.contact__input--buffer');
